2bdc907a92c2670e7e2b8f2897fa33b0130883c9
Mark Jones <mark.b.jones@sogeti.com>
2020-11-19 16:00:44 +0000

    update packages

---
+++ src/test/java/com/sogeti/autotest/core/SogetiTest.java
@@ -0,0 +1,21 @@
+package com.sogeti.autotest.core;
+
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features",//path to the features	
+		plugin = {"html:target/cucumber-html-report", "json:target/cucumber-json-report.json" },
+		glue = { "com.sogeti.autotest.steps", "com.sogeti.autotest.model",  "com.sogeti.autotest.core" }
+		)
+public class SogetiTest {
+	public void startTest(){
+		System.out.println("Starting Tests....");
+	}
+		
+}
--- src/test/resources/features/ProductPage.feature
+++ src/test/resources/features/ProductPage.feature
@@ -1,4 +1,4 @@
+@tmp
 Feature: Add Report to Basket
-  @tmp
   Scenario: Adding a Free Item to Basket
     	And I navigate to 'UK' homepage
